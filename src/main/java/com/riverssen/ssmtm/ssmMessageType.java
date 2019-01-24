50bd33ca5b5e134d943825242e3d2cb5a4f1e2d2
Ragnarr Ivarssen <Riverssen@gmail.com>
2019-01-24 22:09:14 +0100

    created a messagetype class.

---
+++ src/main/java/com/riverssen/ssmtm/ssmMessageType.java
@@ -0,0 +1,9 @@
+package com.riverssen.ssmtm;
+
+public class ssmMessageType
+{
+    public static final int
+    NOTIFICATION_MESSAGE = 0,
+    MESSAGE_REQUIRES_REPLY = 1,
+    MESSAGE_WITH_RECEIVE_NOTICE = 2;
+}
